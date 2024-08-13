153a7ddd489f48c05bb7804e6c16ac692159264f
Zerossive <47612836+Zerossive@users.noreply.github.com>
2024-08-12 20:22:41 -0500

    feat: update yazi

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,3 +1,67 @@
 require("session"):setup({
 	sync_yanked = true,
 })
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	-- type = ui.Border.PLAIN,
+})
+local catppuccin_theme = require("yatline-catppuccin"):setup("mocha") -- or "latte" | "frappe" | "macchiato"
+require("yatline"):setup({
+	theme = catppuccin_theme,
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
+
+	show_background = false,
+
+	display_header_line = true,
+	display_status_line = true,
+
+	tab_use_inverse = true,
+
+	header_line = {
+		left = {
+			section_a = {
+				{ type = "line", custom = false, name = "tabs", params = { "left" } },
+			},
+			section_b = {},
+			section_c = {},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "date", params = { "%A, %d %B %Y" } },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "date", params = { "%X" } },
+			},
+			section_c = {},
+		},
+	},
+
+	status_line = {
+		left = {
+			section_a = {
+				{ type = "string", custom = false, name = "tab_mode" },
+			},
+			section_b = {
+				{ type = "string", custom = false, name = "hovered_size" },
+			},
+			section_c = {
+				{ type = "string", custom = false, name = "hovered_name" },
+				{ type = "coloreds", custom = false, name = "count" },
+			},
+		},
+		right = {
+			section_a = {
+				{ type = "string", custom = false, name = "cursor_position" },
+			},
+			section_b = {
+				-- { type = "string", custom = false, name = "cursor_percentage" },
+				{ type = "string", custom = false, name = "hovered_file_extension", params = { true } },
+			},
+			section_c = {
+				{ type = "coloreds", custom = false, name = "permissions" },
+			},
+		},
+	},
+})
