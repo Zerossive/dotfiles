4b6f633d991517edb4b0736008cec2f8d49ffeaa
Zerossive <47612836+Zerossive@users.noreply.github.com>
2024-08-12 20:23:09 -0500

    feat: add wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,54 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "Catppuccin Mocha"
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+
+custom.background = "#1e1e2e"
+custom.tab_bar.background = "#1e1e2e"
+custom.tab_bar.active_tab.bg_color = "#74c7ec"
+custom.tab_bar.active_tab.intensity = "Bold"
+custom.tab_bar.inactive_tab.bg_color = "#1e1e2e"
+custom.tab_bar.inactive_tab_hover.bg_color = "#45475A"
+custom.tab_bar.new_tab.bg_color = "#1e1e2e"
+custom.tab_bar.new_tab_hover.bg_color = "#45475A"
+
+config.command_palette_bg_color = "#181825"
+config.command_palette_fg_color = "#BAC2DE"
+
+config.color_schemes = { ["Catppuccin Mocha Sapphire"] = custom }
+config.color_scheme = "Catppuccin Mocha Sapphire"
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 30
+
+config.background = {
+	{
+		source = { File = "/home/danny/Elysium/Images/Catppuccin/smoke-dark.png" },
+		height = "Cover",
+		horizontal_align = "Center",
+	},
+}
+
+-- config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+
+config.window_padding = {
+	-- left = 0,
+	-- right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.default_cursor_style = "SteadyBar"
+config.force_reverse_video_cursor = true
+config.adjust_window_size_when_changing_font_size = false
+
+-- and finally, return the configuration to wezterm
+return config
