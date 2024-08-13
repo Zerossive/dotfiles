0c8511dbfea7ea2decbce090289baa043069c3ba
Zerossive <47612836+Zerossive@users.noreply.github.com>
2024-08-12 20:21:12 -0500

    feat: update install script

---
+++ install-script.sh
@@ -0,0 +1,190 @@
+#!/bin/sh
+
+PACKAGE_LIST="audacity
+blender
+bottles
+boxes
+corectrl
+firefox
+fragments
+inkscape
+krita
+lutris
+picard
+obs-studio
+openrgb
+piper
+protontricks
+syncthing
+vlc
+flatpak
+steam
+discord
+jellyfin
+cava"
+
+FLATPAK_LIST="com.github.qarmin.czkawka
+com.belmoussaoui.Decoder
+org.nickvision.money
+com.github.tenderowl.frog
+fr.handbrake.ghb
+com.heroicgameslauncher.hgl
+org.gnome.design.Lorem
+io.github.seadve.Mousai
+md.obsidian.Obsidian
+org.nickvision.tubeconverter
+community.pathofbuilding.PathOfBuilding
+org.gnome.World.PikaBackup
+net.davidotek.pupgui2
+io.gitlab.theevilskeleton.Upscaler
+io.github.flattool.Warehouse"
+
+BREW_LIST="yazi"
+
+TERMINAL_LIST="bat
+btop
+fd-find
+figlet
+fzf
+git
+fastfetch
+neovim
+npm
+nvtop
+ripgrep
+tldr
+thefuck
+zoxide
+eza
+duf
+speedtest-cli
+entr
+stow
+trash-cli"
+
+OTHER_LIST="feishin
+vscodium
+portmaster
+nomachine
+awakened poe trade
+brave
+vivaldi
+scc"
+
+list_apps() {
+	local category="${1:-all}"
+	if [ $category == "package" ]; then
+		echo "$PACKAGE_LIST"
+	elif [ $category == "flatpak" ]; then
+		echo "$FLATPAK_LIST"
+	elif [ $category == "brew" ]; then
+		echo "$BREW_LIST"
+	elif [ $category == "terminal" ]; then
+		echo "$TERMINAL_LIST"
+	elif [ $category == "other" ]; then
+		echo "$OTHER_LIST"
+	elif [ $category == "all" ]; then
+		echo "$PACKAGE_LIST"
+		echo "$FLATPAK_LIST"
+		echo "$BREW_LIST"
+		echo "$TERMINAL_LIST"
+		echo "$OTHER_LIST"
+	else
+		echo "Invalid argument (valid arguments: package, flatpak, brew, terminal, other, all)"
+	fi
+}
+
+# Installers
+packageInstall() {
+	echo "Installing packages..."
+	sudo dnf install $1
+}
+flatpakInstall() {
+	if command -v flatpak &> /dev/null; then
+		echo "Installing flatpak apps..."
+		flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+		sudo flatpak install $1
+	else
+		echo "Error: flatpak not found"
+	fi
+}
+brewInstall() {
+	if command -v brew &> /dev/null; then
+		echo "Installing brew apps..."
+		brew install $1
+	else
+		echo "Error: brew not found"
+	fi
+}
+
+install_apps() {
+	local category="${1:-all}"
+	if [ $category == "package" ]; then
+		packageInstall "$PACKAGE_LIST"
+	elif [ $category == "flatpak" ]; then
+		flatpakInstall "$FLATPAK_LIST"
+	elif [ $category == "brew" ]; then
+		brewInstall "$BREW_LIST"
+	elif [ $category == "terminal" ]; then
+		packageInstall "$TERMINAL_LIST"
+	elif [ $category == "all" ]; then
+		packageInstall "$PACKAGE_LIST"
+		flatpakInstall "$FLATPAK_LIST"
+		brewInstall "$BREW_LIST"
+		packageInstall "$TERMINAL_LIST"
+	else
+		echo "Invalid argument (valid arguments: package, flatpak, brew, terminal, all)"
+	fi
+}
+
+help() {
+	cat <<"EOF"
+▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+▏Install Script▕
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Syntax: install-script.sh [-h|l|i]
+
+-l	LIST applications by category
+	Requires one of the following arguments:
+		package, flatpak, brew, terminal, other, all
+	Example:
+		install-script.sh -l all
+	Example:
+		install-script.sh -l package | OTHER_COMMANDS
+
+-i	INSTALL applications by category
+	Requires one of the following arguments:
+		package, flatpak, brew, terminal, all
+	Example:
+		install-script.sh -i package
+
+-h	Print this help page
+EOF
+}
+
+while getopts ":hl:i:" option; do
+	case "$option" in
+	h) # First: check for help
+		help
+		exit
+		;;
+	\?) # Second: check for invalid options
+		echo "Error: Invalid option"
+		exit
+		;;
+	l) # List apps by category
+		list_apps "$OPTARG"
+		exit
+		;;
+	i) # Install apps by category
+		install_apps "$OPTARG"
+		exit
+		;;
+	*)
+		echo "Error: Invalid option, or missing arguments"
+		exit
+		;;
+	esac
+done
+
+help
