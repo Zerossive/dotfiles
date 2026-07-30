2f08544360824b5d6f1ca7268bae8dd6cdb95ff4
Danny <47612836+Zerossive@users.noreply.github.com>
2026-07-30 17:11:23 -0500

    feat(neovim): update bullets config

--- neovim/.config/nvim/lua/plugins/bullets.lua
+++ neovim/.config/nvim/lua/plugins/bullets.lua
@@ -1,6 +1,6 @@
 vim.pack.add { 'https://github.com/bullets-vim/bullets.nvim' }
 
 require('bullets').setup {
-	checkbox_markers = '                   x',
-	outline_levels = { 'ROM', 'ABC', 'num', 'abc', 'rom', 'std-' },
+	checkbox_markers = ' x',
+	outline_levels = { 'std-' },
 }
